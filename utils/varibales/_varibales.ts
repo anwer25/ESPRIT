dd2cae7bafd426f9f3ffff7630e2b06868d24832
anwer <anwern29@outlook.fr>
2022-01-23 00:32:49 +0100

    start on task one

--- utils/varibales/_varibales.ts
+++
@@ -1,6 +0,0 @@
-import colorsName from "../colors/colors.name";
-
-const defaultNavContainerColor = colorsName.blueBg.hex;
-
-// eslint-disable-next-line import/prefer-default-export
-export { defaultNavContainerColor };
