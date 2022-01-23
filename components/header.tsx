7f784f5dd9fe06e635f43b6b9570e6d79f031e4f
anwerDev <anwern29@outlook.fr>
2022-01-23 14:45:33 +0100

    add theme and fix some issue

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import {
+	NavContainer,
+	CercleContainer,
+	CercleOne,
+	CercleTow,
+} from "./styledComponents/views";
+import NavComponent from "./nav";
 
-const Header: React.FC = (): JSX.Element => {};
+const Header: React.FC = (): JSX.Element => (
+	<NavContainer>
+		<NavComponent />
+		<CercleContainer>
+			<CercleOne>
+				<CercleTow />
+			</CercleOne>
+		</CercleContainer>
+	</NavContainer>
+);
 
 export default Header;
--- hooks/theme.tsx
+++ hooks/theme.tsx
@@ -1,10 +1,11 @@
 import React, { useState, createContext, useMemo } from "react";
+import { defaultTheme } from "../utils/varibales/_varibales";
 
 export const ThemeContext = createContext({});
 
 // eslint-disable-next-line no-undef
 const UseThemeProviderComponent: React.FC = ({ children }): JSX.Element => {
-	const [theme, setTheme] = useState({});
+	const [theme, setTheme] = useState(defaultTheme);
 	const themeMemo = useMemo(() => ({ theme, setTheme }), [theme]);
 	return (
 		<ThemeContext.Provider value={themeMemo}>
